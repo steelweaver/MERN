427be9ce98f92c09189bd7b744388c09aaf61ef3
steelweaver <47039527+steelweaver@users.noreply.github.com>
2021-09-01 15:03:22 -0400

    Create emisupport_create.ps1

---
+++ emisupport_create.ps1
@@ -0,0 +1,14 @@
+$user = "emisupport"
+Remove-LocalUser -Name $user
+
+
+$Password = Read-Host -AsSecureString
+
+New-LocalUser $user -Password $Password -FullName $user -Description "Compte administrateur local pour le dépannage"
+
+Add-LocalGroupMember -Group "Administrators" -Member $user
+
+Add-LocalGroupMember -Group "Administrators" -Member $user
+Add-LocalGroupMember -Group "Administrateurs" -Member $user
+
+Set-LocalUser -Name $user -PasswordNeverExpires 1
